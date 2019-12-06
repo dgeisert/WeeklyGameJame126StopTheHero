968bee07c099a6abcfb52cd5f27f6368d648cccb
summitdavid <dg@summitstartups.com>
2019-12-06 09:21:41 -0800

    Base

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public static Controls Instance;
+    public static bool Next
+    {
+        get
+        {
+            return Input.GetKeyDown(KeyCode.Space) ||
+                Input.GetKeyDown(KeyCode.W) ||
+                Input.GetKeyDown(KeyCode.UpArrow);
+        }
+    }
+    public static bool Pause
+    {
+        get
+        {
+            return Input.GetKeyDown(KeyCode.P);
+        }
+    }
+}
